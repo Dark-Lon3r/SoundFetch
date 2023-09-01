c6a8b2ed27ad941d04a2ff50d08535647a4417ba
Dark-Lon3r <artempro4900@gmail.com>
2023-09-01 10:18:31 +0300

    <Added the ability to download TikTok videos>

--- main.py
+++ main.py
@@ -1,39 +1,71 @@
 from bs4 import BeautifulSoup as BS
 import requests
-import threading
+
+
+HEADER = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}
+
 
 def sound_cloud_downdload(link_sound_cloud):
-	header = {
-	    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-	}
+    data_link = {'value': link_sound_cloud}
+
+    try:
+        r = requests.post('https://www.klickaud.co/download.php',
+                          data=data_link, headers=HEADER)
+        soup = BS(r.text, 'html.parser')
+
+        titles = soup.find('td', class_='no-mobile1')
+        name = soup.find_all('td', class_='small-10 columns')[1].text.strip()
+        link_music = titles.find('a').get('href')
+        photo_link = soup.find('td', class_='small-10 columns')
+
+        # Music
+        r = requests.get(link_music)
+        with open(f"{name}.mp3", "wb") as file:
+            file.write(r.content)
+
+        # Photo
+        r_photo = requests.get(photo_link.find('img')['src'])
+        with open(f"{name}.jpg", "wb") as file_photo:
+            file_photo.write(r_photo.content)
+
+    except:
+        print('😕Error!')
 
 
-	data_link = {'value': link_sound_cloud}
+def tiktok_download(link):
+    data_link = {
+        'id': link
+    }
 
-	try:
-		r = requests.post('https://www.klickaud.co/download.php', data=data_link, headers=header)
-		soup = BS(r.text, 'html.parser')
+    r = requests.post(
+        'https://ttsave.app/download?mode=video&key=c679452f-fbe4-44ca-827f-8ac7565b0143', data=data_link, headers=HEADER)
 
-		titles = soup.find('td' ,class_ = 'no-mobile1')
-		name = soup.find_all('td' ,class_ = 'small-10 columns')[1].text.strip()
-		link_music = titles.find('a').get('href')
-		photo_link = soup.find('td', class_ = 'small-10 columns')
-	        
-		#Загрузка трека
-		r = requests.get(link_music)
-		with open(f"{name}.mp3", "wb") as file:
-			file.write(r.content)
+    soup = BS(r.text, 'html.parser')
+    download_link_div = soup.find('div', {'id': 'button-download-ready'})
 
-		#Загрузка фото
-		r_photo = requests.get(photo_link.find('img')['src'])
-		with open(f"{name}.jpg", "wb") as file_photo:
-			file_photo.write(r_photo.content)
+    download_link_a = download_link_div.find('a')
+    name = soup.find(
+        'a', class_='font-extrabold text-blue-400 text-xl mb-2').text.strip()
+    if download_link_a:
+        download_url = download_link_a['href']
+        r = requests.get(download_url)
 
+        with open(f"{name}.mp4", "wb") as file:
+            file.write(r.content)
 
-	except:
-		print('😕Ошибка!')
+    else:
+        print('😕Error!')
 
 
-if __name__ in "__main__":
-	link_sound_cloud = input("Enter URL--->  ")
-	threading.Thread(target=sound_cloud_downdload(link_sound_cloud))
+def check_link(link):
+    if "https://www.tiktok.com/" in link or "https://vm.tiktok.com/" in link:
+        tiktok_download(link)
+
+    else:
+        sound_cloud_downdload(link)
+
+
+if __name__ == "__main__":
+    link = input("Enter URL---> ")
+    check_link(link)
